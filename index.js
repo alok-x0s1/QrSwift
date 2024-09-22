c82d1192dbde931730ab5ed5136c6d81342ec850
Alok Yadav <yalok6321@gmail.com>
2024-09-22 18:46:10 +0530

    Add event on submiting form and ui change

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-const inputValue = document.querySelector('#inputValue');
-const divImage = document.querySelector('#divImage');
-const imgSrc = document.querySelector('#imgSrc');
+const inputValue = document.querySelector("#inputValue");
+const divImage = document.querySelector("#divImage");
+const imgSrc = document.querySelector("#imgSrc");
+const form = document.getElementById("form");
 
-const qrCodeGenerator = () => {
-    if(inputValue.value.length > 0) {
-        imgSrc.src = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + inputValue.value;
-        divImage.classList.add('show-img');
-        document.querySelector('.links').style.visibility = 'visible';
-    } else {
-        inputValue.classList.add('error');
-        setTimeout(()=> {
-            inputValue.classList.remove('error');
-        }, 1000)
-    }
-}
+const qrCodeGenerator = (e) => {
+	e.preventDefault();
+	inputValue.classList.remove("error");
+	divImage.classList.remove("show-img");
+	if (inputValue.value.length > 0) {
+		imgSrc.src =
+			"https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+			inputValue.value;
+		divImage.classList.add("show-img");
+		document.querySelector(".links").style.visibility = "visible";
+	} else {
+		inputValue.classList.add("error");
+		setTimeout(() => {
+			inputValue.classList.remove("error");
+		}, 1000);
+	}
+};
+
+form.addEventListener("submit", qrCodeGenerator);
